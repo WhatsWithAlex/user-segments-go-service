7dffcd584498a6657b887092d35987145830754a
WhatsWithAlex <kukuruzaofvictory@gmail.com>
2023-08-31 22:00:40 +0300

    feat: migrate up on startup

--- internal/app/db.go
+++ internal/app/db.go
@@ -2,11 +2,15 @@ package app
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/WhatsWithAlex/user-segments-go-service/internal/env"
 	"github.com/WhatsWithAlex/user-segments-go-service/internal/postgresdb"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -17,14 +21,32 @@ func setupDB(env *env.DBEnv) (db *postgresdb.Store, err error) {
 	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
 		env.User,
 		env.Password,
-		env.Addr,
+		env.Host,
 		env.Port,
 		env.Name,
 	)
+
+	err = runMigrate(connString)
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return
+	}
+
 	dbConn, err := pgxpool.New(ctx, connString)
 	if err != nil {
 		return
 	}
 	db = postgresdb.NewStore(dbConn)
 	return
+}
+
+func runMigrate(connString string) error {
+	m, err := migrate.New(
+		"file://./sql/migrations",
+		connString,
+	)
+	if err != nil {
+		return err
+	}
+	err = m.Up()
+	return err
 }
